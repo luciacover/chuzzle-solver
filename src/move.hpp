1bffee17eaf0fe1e64a0d90368d097e4aa44aba9
luciacover <llcsaturnaliam@gmail.com>
2025-10-08 18:25:37 -0400

    moved move constructor to cpp file

--- src/move.hpp
+++ src/move.hpp
@@ -19,21 +19,7 @@ class Move {
     Move(const table_t &table, const board_t &initial, const hashsize_t &board_hash,
          const std::optional<std::tuple<char, u8, u8>> &mod,
          std::shared_ptr<std::unordered_set<hashsize_t>> previously_generated,
-         const board_t &goal) : move_hash(board_hash), modification(mod) {
-      previously_generated->insert(this->move_hash);
-
-      if (this->modification) return;
-
-      // simple test to make sure the shared ptr stuff works right
-      for (int i = 0; i < 10; i++) {
-        const board_t b = random_board();
-        const hashsize_t h = hash(b, table);
-        const auto t = std::make_tuple('c', 3, 4);
-        const Move nm(table, initial, h, t, previously_generated, goal);
-        const auto np = std::make_shared<Move>(nm);
-        this->next_moves.push_back(np);
-      }
-    }
+         const board_t &goal);
     ~Move() {}
 };
 
